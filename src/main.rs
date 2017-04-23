6feb44ececbf8460fdb57a6b463e8eadf0793b49
Chabare <chabare95@gmail.com>
2017-04-23 18:33:31 +0200

    Use individual functions

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,9 @@ use regex::Regex;
 
 use std::fs;
 use std::fs::File;
-use std::path::Path;
-use std::process::Command;
+use std::path::{Path, PathBuf};
+use std::process::{Command, ExitStatus};
+use std::io;
 use std::io::{Read, Write};
 use std::ops::Sub;
 
@@ -23,13 +24,13 @@ fn main() {
     let start = time::now();
 
     let app = get_command_line_app();
+    let matches = app.get_matches();
 
     let base_url = "http://support.yealink.com/documentFront/forwardToDocumentDetailPage?documentId=";
     let t23p_id = 33;
     let url = get_device_url(base_url, t23p_id);
 
     // Target directory has a default value -> Safe usage of unwrap
-    let matches = app.get_matches();
     let target_directory = get_target_directory(&matches);
     let remove_zip = matches.is_present("Remove zip");
 
@@ -37,73 +38,103 @@ fn main() {
     let client = get_client();
     let body: String = get_body(url.as_str(), &client);
 
-    let regex = Regex::new("<a href=\"(?P<link>.*\\.zip)\".*\\n\\s*<span class=\"firm-new").expect("Failed to compile new firmware regex.");
-    let regex_match: Option<regex::Match> = match regex.find(body.as_str()) {
+    let new_firmware_regex = Regex::new("<a href=\"(?P<link>.*\\.zip)\".*\\n\\s*<span class=\"firm-new").expect("Failed to compile new firmware regex.");
+    let regex_match: Option<regex::Match> = match new_firmware_regex.find(body.as_str()) {
         Some(val) => { Some(val) }
         None => get_new_firmware(body.as_str())
     };
 
-    let captures: Option<regex::Captures> = get_captures(&regex, regex_match);
+    let captures: Option<regex::Captures> = get_captures(&new_firmware_regex, regex_match);
 
     if captures.is_some() {
-        let link = captures.unwrap().name("link").unwrap().as_str();
-        let response: Option<hyper::client::Response> = match client.get(link).send() {
-            Ok(val) => { Some(val) }
+        let captures = captures.unwrap();
+        let link = captures.name("link").unwrap().as_str();
+
+        let file_content = get_firmware(&link, &client);
+
+        let filename = get_filename(link);
+        let path = get_path(filename, &target_directory);
+        let mut file = File::create(&path).unwrap();
+
+        let start = time::now();
+        match write_file(&mut file, file_content) {
+            Ok(size) => {
+                let end = time::now().sub(start);
+                let path = path.as_path().to_str().unwrap();
+                let time = format!("{}.{}s", end.num_seconds(), end.num_milliseconds());
+                let size = convert(size as f64);
+                println!("Successfully wrote `{}` with {} from `{}` in {}.", path, size, link, time)
+            }
             Err(e) => {
-                println!("Couldn't read data from `{:?}` due to an error: {:?}", link, e);
-                None
+                println!("Writing file `{}` failed due to error: {:?}", filename, e)
+            }
+        }
+
+        let output_path = path.parent().unwrap().to_str().unwrap();
+        println!("Unzipping {} to {}", path.to_str().unwrap(), output_path);
+        match unzip(&path, output_path) {
+            Ok(_) => {
+                let end = time::now().sub(start);
+                println!("Finished unzipping in {}.{}s", end.num_seconds(), end.num_milliseconds());
+
+                if remove_zip {
+                    match fs::remove_file(&path) {
+                        Ok(_) => { println!("Successfully deleted .zip file.") }
+                        Err(e) => println!("Failed to delete .zip file: {:?}", e)
+                    };
+                }
+            }
+            Err(e) => {
+                println!("Failed unzipping file ({}) due to error: {:?}", path.to_str().unwrap(), e);
             }
         };
-
-        if response.is_some() {
-            let mut response = response.unwrap();
-            let mut file_content = Vec::new();
-
-            let start = time::now();
-            match response.read_to_end(&mut file_content) {
-                Ok(size) => {
-                    let end = time::now().sub(start);
-                    let time = format!("{}.{:.2}s", end.num_seconds(), end.num_milliseconds());
-                    println!("Successfully read binary data from `{}` with size {} in {}", link, convert(size as f64), time)
-                }
-                Err(error) => println!("Downloading firmware from `{}` failed due to error: {:?}", link, error)
-            };
-
-            let filename = link.rsplit("%2F").nth(0).unwrap();
-            let path = Path::new(&target_directory).join(filename);
-            let mut file = File::create(&path).unwrap();
-            let start = time::now();
-            match file.write(file_content.as_slice()) {
-                Ok(size) => {
-                    let end = time::now().sub(start);
-                    println!("Successfully wrote `{}` with {} from `{}` in {}.{}s.", path.as_path().to_str().unwrap(), convert(size as f64), link, end.num_seconds(), end.num_milliseconds())
-                }
-                Err(e) => println!("Writing file `{}` failed due to error: {:?}", filename, e)
-            };
-
-            let output_path = match path.parent() {
-                Some(val) => { val.to_str().unwrap() }
-                None => target_directory.as_str()
-            };
-            println!("Unzipping {} to {}", path.to_str().unwrap(), output_path);
-            let start = time::now();
-            Command::new("unzip")
-                .args(&["-n", path.to_str().unwrap(), "-d", output_path])
-                .status()
-                .expect("Failed to unzip archive.");
-            let end = time::now().sub(start);
-            if remove_zip {
-                match fs::remove_file(&path) {
-                    Ok(_) => { println!("Successfully deleted .zip file.") }
-                    Err(e) => println!("Failed to delete .zip file: {:?}", e)
-                };
-            }
-            println!("Finished unzipping in {}.{}s", end.num_seconds(), end.num_milliseconds());
-        }
     }
 
     let end = time::now().sub(start);
     println!("Finished execution in {}.{}s", end.num_seconds(), end.num_milliseconds());
+}
+
+fn unzip<'a>(path: &Path, output_path: &'a str) -> io::Result<ExitStatus> {
+    Command::new("unzip")
+        .args(&["-n", path.to_str().unwrap(), "-d", output_path])
+        .status()
+}
+
+fn get_filename(link: &str) -> &str {
+    link.rsplit("%2F").nth(0).unwrap()
+}
+
+fn get_path<'a>(filename: &'a str, target_directory: &String) -> PathBuf {
+    Path::new(&target_directory).join(filename)
+}
+
+fn get_firmware<'a>(link: &'a str, client: &Client) -> Vec<u8> {
+    let response: Option<hyper::client::Response> = match client.get(link).send() {
+        Ok(val) => { Some(val) }
+        Err(e) => {
+            println!("Couldn't read data from `{:?}` due to an error: {:?}", link, e);
+            None
+        }
+    };
+
+    let mut response = response.unwrap();
+    let mut file_content = Vec::new();
+
+    let start = time::now();
+    match response.read_to_end(&mut file_content) {
+        Ok(size) => {
+            let end = time::now().sub(start);
+            let time = format!("{}.{:.2}s", end.num_seconds(), end.num_milliseconds());
+            println!("Successfully read binary data from `{}` with size {} in {}", link, convert(size as f64), time)
+        }
+        Err(error) => println!("Downloading firmware from `{}` failed due to error: {:?}", link, error)
+    };
+
+    file_content
+}
+
+fn write_file(file: &mut File, content: Vec<u8>) -> io::Result<usize> {
+    file.write(content.as_slice())
 }
 
 fn get_captures<'a>(regex: &Regex, regex_match: Option<regex::Match<'a>>) -> Option<regex::Captures<'a>> {
