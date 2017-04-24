2fae72f7a1504baaa3654bd37434b67c35b74a9e
Chabare <chabare95@gmail.com>
2017-04-24 23:13:46 +0200

    Fix deploy script

--- deploy.py
+++ deploy.py
@@ -7,21 +7,14 @@ except IndexError:
   print("Please specify a file to upload as cmd argument")
   sys.exit(2)
 
-s3 = boto3.client('s3')
+s3 = boto3.resource('s3')
 bucket_name = "yealink-firmware"
+bucket = s3.Bucket(bucket_name)
 
-buckets = [bucket['Name'] for bucket in s3.list_buckets() if
-           bucket_name['Name'] == bucket_name]
+try:
+  short_name = file.split("-")[0]
+except IndexError:
+  print("Using filename({}) as key.".format(file))
+  short_name = file.split(".rom", maxsplit=1)[0]
 
-if buckets:
-  bucket = buckets[0]
-  try:
-    short_name = file.split("-")[0]
-  except IndexError:
-    print("Using filename({}) as key.".format(file))
-    short_name = file.split(".rom", maxsplit=1)[0]
-
-  bucket.upload_file(file, '{}.rom'.format(short_name))
-else:
-  print("Couldn't find bucket 'yealink-firmware', aborting.")
-  sys.exit(3)
+bucket.upload_file(file, '{}.rom'.format(short_name))
