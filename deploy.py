e1f8cb1510ce47e75abd304a32520cea2d251600
chabare <chabare95@gmail.com>
2017-04-26 17:38:47 +0200

    Use python comments

--- deploy.py
+++ deploy.py
@@ -16,7 +16,7 @@ except IndexError:
 
 region_name = "eu-central-1"
 config = botocore.config.Config(region_name=region_name, signature_version='v4')
-// see http://boto3.readthedocs.io/en/latest/guide/configuration.html for setting the credentials
+# see http://boto3.readthedocs.io/en/latest/guide/configuration.html for setting the credentials
 s3 = boto3.resource('s3', region_name=region_name, config=config)
 bucket = s3.Bucket(bucket_name)
 
