2856052c8d9f6c2fced3ce1e002b82decec9b98c
chabare <chabare95@gmail.com>
2017-04-26 17:37:49 +0200

    Option to supply bucket-name via cmd

--- deploy.py
+++ deploy.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import sys
 
 try:
@@ -7,8 +8,16 @@ except IndexError:
   print("Please specify a file to upload as cmd argument")
   sys.exit(2)
 
-s3 = boto3.resource('s3', region_name="eu-central-1")
-bucket_name = "yealink-firmware"
+try:
+  bucket_name = sys.argv[2]
+except IndexError:
+  print("Using 'yealink-firmware'" as bucket-name")
+  bucket_name = yealink-firmware
+
+region_name = "eu-central-1"
+config = botocore.config.Config(region_name=region_name, signature_version='v4')
+// see http://boto3.readthedocs.io/en/latest/guide/configuration.html for setting the credentials
+s3 = boto3.resource('s3', region_name=region_name, config=config)
 bucket = s3.Bucket(bucket_name)
 
 try:
