290aead31c704ec3cf366fca36ee5ecd07abfa09
chabare <chabare95@gmail.com>
2017-05-04 16:17:13 +0200

    Add outputs to deployment

--- deploy.py
+++ deploy.py
@@ -1,29 +1,29 @@
-import boto3
 import sys
 
-try:
-  file = sys.argv[1]
-except IndexError:
-  print("Please specify a file to upload as cmd argument")
-  sys.exit(2)
+import boto3
 
 try:
-  bucket_name = sys.argv[2]
+    file = sys.argv[1]
 except IndexError:
-  print("Using 'yealink-firmware' as bucket-name")
-  bucket_name = "yealink-firmware"
+    print("Please specify a file to upload as cmd argument")
+    sys.exit(2)
+
+try:
+    bucket_name = sys.argv[2]
+except IndexError:
+    print("Using 'yealink-firmware' as bucket-name")
+    bucket_name = "yealink-firmware"
 
 region_name = "eu-central-1"
 # config = botocore.config.Config(region_name=region_name, signature_version='v4')
 # see http://boto3.readthedocs.io/en/latest/guide/configuration.html for setting the credentials
 # s3 = boto3.resource('s3', region_name=region_name, config=config)
 s3 = boto3.resource('s3', region_name=region_name)
-bucket = s3.Bucket(bucket_name)
+bucket: s3.Bucket = s3.Bucket(bucket_name)
 
-try:
-  short_name = file.split("-")[0]
-except IndexError:
-  print("Using filename({}) as key.".format(file))
-  short_name = file.split(".rom", maxsplit=1)[0]
+short_name = file.split("-")[0]
+print("Using {} as key".format(short_name))
 
 bucket.upload_file(file, '{}.rom'.format(short_name))
+print("Successfully uploaded file {}".format(short_name))
+print("URL: {}".format(f"https://s3.{region_name}.amazonaws.com/{bucket_name}/{short_name}"))
