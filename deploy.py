571c3fd3c161d7ad7358b49aa46cb243e5aa86e2
chabare <chabare95@gmail.com>
2017-05-15 23:14:00 +0200

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,18 +1,17 @@
+import boto3
 import sys
 
-import boto3
+try:
+  file = sys.argv[1]
+except IndexError:
+  print("Please specify a file to upload as cmd argument")
+  sys.exit(2)
 
 try:
-    file = sys.argv[1]
+  bucket_name = sys.argv[2]
 except IndexError:
-    print("Please specify a file to upload as cmd argument")
-    sys.exit(2)
-
-try:
-    bucket_name = sys.argv[2]
-except IndexError:
-    print("Using 'yealink-firmware' as bucket-name")
-    bucket_name = "yealink-firmware"
+  print("Using 'yealink-firmware' as bucket-name")
+  bucket_name = "yealink-firmware"
 
 region_name = "eu-central-1"
 # config = botocore.config.Config(region_name=region_name, signature_version='v4')
@@ -26,4 +25,5 @@ print("Using {} as key".format(short_name))
 
 bucket.upload_file(file, '{}.rom'.format(short_name))
 print("Successfully uploaded file {}".format(short_name))
-print("URL: {}".format(f"https://s3.{region_name}.amazonaws.com/{bucket_name}/{short_name}"))
+print("URL: {}".format(
+  f"https://s3.{region_name}.amazonaws.com/{bucket_name}/{short_name}"))
